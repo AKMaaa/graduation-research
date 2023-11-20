b5878927a981d79828bd4c4de85c57bfd1b3be1d
Akira_Maeda <akiramaeda0905@gmail.com>
2023-11-20 16:10:57 +0900

    start

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -8,69 +8,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Stylish Login',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo UI',
+      theme: ThemeData.light(),
+      home: AdminMobilePage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
+class AdminMobilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Colors.blue, Colors.white],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlutterLogo(size: 70),
-              SizedBox(height: 40),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                  prefixIcon: Icon(Icons.account_circle),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  // ログイン処理をここに追加
-                },
-                child: Text('Loginだよねぇ'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: Row(
+        children: [
+          /*ここにWidgetを並べていく*/
+        ],
       ),
     );
   }
